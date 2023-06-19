9085886fc6862ae5e323ff92f2c8be320ff2df5f
Jas Dosanjh <jas@dosanjh.co.uk>
2023-06-19 20:13:56 +0100

    benefits section && improved types docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import NavBar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -33,6 +34,7 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,6 +2,11 @@
  * Enum representing the selected page.
  *
  * @enum {string}
+ * @readonly
+ * @property {string} Home - The home page.
+ * @property {string} Benefits - The benefits page.
+ * @property {string} Classes - The classes page.
+ * @property {string} ContactUs - The contact us page.
  */
 
 export enum SelectedPage {
@@ -9,4 +14,18 @@ export enum SelectedPage {
   Benefits = "benefits",
   Classes = "classes",
   ContactUs = "contactus",
+}
+
+/**
+ * Enum representing the benefit type.
+ *
+ * @typedef {Object} BenefitType
+ * @property {JSX.Element} icon - The icon element for the benefit.
+ * @property {string} title - The title of the benefit.
+ * @property {string} description - The description of the benefit.
+ */
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
 }
